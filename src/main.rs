1c8849ad3b24d1ee1714118d4858897c2e1c1e94
gauthier13 <gauthierleclair1@outlook.fr>
2025-10-04 18:48:46 +0200

    Add cors

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,29 @@ mod routes;
 use axum::{routing::get, Router};
 use std::net::SocketAddr;
 
+use tower_http::{
+    cors::{Any, CorsLayer},
+    trace::TraceLayer,
+};
+
 #[tokio::main]
 async fn main() {
     // set logging
-    tracing_subscriber::fmt::init();
+    tracing_subscriber::fmt()
+        .with_target(false)
+        .compact()
+        .init();
 
     // build router
-    let app = Router::new().route("/", get(routes::health::health_check));
+    let app = Router::new()
+        .route("/", get(routes::health::health_check))
+        .layer(
+            CorsLayer::new()
+                .allow_origin(Any)
+                .allow_methods(Any)
+                .allow_headers(Any),
+        )
+        .layer(TraceLayer::new_for_http());
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 9000));
     tracing::info!("🚀Server listening on {}", addr);
